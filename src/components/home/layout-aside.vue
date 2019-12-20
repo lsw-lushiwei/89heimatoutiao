ec0a4eab0409670bfad7222ca09f9a2069da0d8c
lushiwei <lushiwei0822@163.com>
2019-12-20 11:46:54 +0800

    主页模块-左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -3,30 +3,27 @@
     <div class="title">
       <img src="../../assets/img/logo_admin.png" alt />
     </div>
-    <el-menu
-      style="width:231px"
-      background-color="#353b4e"
-      text-color="#adafb5"
-      active-text-color="#ffd04b"
-    >
-      <el-menu-item>首页</el-menu-item>
+    <!-- 设置宽度、背景颜色、字体颜色、字体激活颜色、开启路由模式 -->
+    <el-menu style="width:231px" background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b" :router="true">
+      <el-menu-item index='/home'>首页</el-menu-item>
+      <!-- 二级导航 -->
       <el-submenu index="1">
         <template slot="title">
           <span>内容管理</span>
         </template>
-        <el-menu-item>发布文章</el-menu-item>
-        <el-menu-item>内容列表</el-menu-item>
-        <el-menu-item>评论列表</el-menu-item>
-        <el-menu-item>素材管理</el-menu-item>
+        <el-menu-item index='/home/publish'>发布文章</el-menu-item>
+        <el-menu-item index='/home/articles'>内容列表</el-menu-item>
+        <el-menu-item index='/home/comment'>评论列表</el-menu-item>
+        <el-menu-item index='/home/material'>素材管理</el-menu-item>
       </el-submenu>
-      <el-submenu index="1">
+      <el-submenu index="2">
         <template slot="title">
           <span>粉丝管理</span>
         </template>
-        <el-menu-item>图文数据</el-menu-item>
-        <el-menu-item>粉丝概况</el-menu-item>
-        <el-menu-item>粉丝画像</el-menu-item>
-        <el-menu-item>粉丝列表</el-menu-item>
+        <el-menu-item index='/home/picture'>图文数据</el-menu-item>
+        <el-menu-item index='/home/fansinfo'>粉丝概况</el-menu-item>
+        <el-menu-item index='/home/fanslife'>粉丝画像</el-menu-item>
+        <el-menu-item index='/home/fanslist'>粉丝列表</el-menu-item>
       </el-submenu>
       <el-menu-item>账户信息</el-menu-item>
     </el-menu>
