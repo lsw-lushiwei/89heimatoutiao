3db8469fe5dc34ad43230dfdf30fc1a3f3ca6773
lushiwei <lushiwei0822@163.com>
2019-12-21 17:35:46 +0800

    面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-breadcrimb>
+    <el-breadcrimb-item to="/home">首页</el-breadcrimb-item>
+    <el-breadcrimb-item>
+        <!-- 具名插槽 -->
+        <slot name='title'></slot>
+    </el-breadcrimb-item>
+  </el-breadcrimb>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
