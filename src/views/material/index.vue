e8603d1a55ed1cdf34127148a782f7dd7647a7a8
lushiwei <lushiwei0822@163.com>
2019-12-22 20:38:08 +0800

    素材列表-数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -6,11 +6,17 @@
     <!-- 素材 -->
     <el-tabs v-model="activeName">
       <el-tab-pane label="全部素材" name="all">
-
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+            <el-row class="operate" type='flex' align="middle" justify="space-around">
+                <i class="el-icon-star-on"></i>
+                <i class="el-icon-delete-solid"></i>
+            </el-row>
+          </el-card>
+        </div>
       </el-tab-pane>
-      <el-tab-pane label="收藏素材" name="collect">
-
-      </el-tab-pane>
+      <el-tab-pane label="收藏素材" name="collect"></el-tab-pane>
     </el-tabs>
   </el-card>
 </template>
@@ -19,11 +25,48 @@
 export default {
   data () {
     return {
-      activeName: 'collect'
+      activeName: 'all', // 默认选中全部
+      list: [] // 接收全部数据
     }
+  },
+  methods: {
+    getAllMaterial () {
+      this.$axios({
+        url: '/user/images',
+        params: { collect: false }
+      }).then(res => {
+        this.list = res.data.results
+      })
+    }
+  },
+  created () {
+    this.getAllMaterial() // 获取所有素材
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.img-list {
+  display: flex;
+  flex-wrap: wrap;
+  .img-card {
+    width: 200px;
+    height: 240px;
+    margin: 20px 50px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .operate{
+        width: 100%;
+        font-size: 20px;
+        position: absolute;
+        bottom: 0;
+        background-color: #f4f5f6;
+        height: 30px;
+        margin-left: -20px;
+    }
+  }
+}
 </style>
