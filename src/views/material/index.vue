917eacb8e8ba16023c57618580b64dc5af00fac6
lushiwei <lushiwei0822@163.com>
2019-12-22 21:04:33 +0800

    素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -4,19 +4,40 @@
       <template slot="title">素材管理</template>
     </bread-crumb>
     <!-- 素材 -->
-    <el-tabs v-model="activeName">
+    <el-tabs v-model="activeName" @tab-click="changeTab">
       <el-tab-pane label="全部素材" name="all">
         <div class="img-list">
           <el-card class="img-card" v-for="item in list" :key="item.id">
             <img :src="item.url" alt />
-            <el-row class="operate" type='flex' align="middle" justify="space-around">
-                <i class="el-icon-star-on"></i>
-                <i class="el-icon-delete-solid"></i>
+            <el-row class="operate" type="flex" align="middle" justify="space-around">
+              <i class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
             </el-row>
           </el-card>
         </div>
+        <el-row type="flex" justify="center" style="height: 80px">
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="page.total"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            @current-change="changePage"
+          ></el-pagination>
+        </el-row>
       </el-tab-pane>
-      <el-tab-pane label="收藏素材" name="collect"></el-tab-pane>
+      <el-tab-pane label="收藏素材" name="collect">
+        <el-row type="flex" justify="center" style="height: 80px">
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="page.total"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            @current-change="changePage"
+          ></el-pagination>
+        </el-row>
+      </el-tab-pane>
     </el-tabs>
   </el-card>
 </template>
@@ -26,16 +47,36 @@ export default {
   data () {
     return {
       activeName: 'all', // 默认选中全部
-      list: [] // 接收全部数据
+      list: [], // 接收全部数据
+      page: {
+        currentPage: 1,
+        pageSize: 8,
+        total: 0
+      }
     }
   },
   methods: {
+    // 切换分页
+    changePage (newPage) {
+      this.page.currentPage = newPage // 得到最新页码
+      this.getAllMaterial()
+    },
+    changeTab () {
+      // 应该吧当前页码回到第一页
+      this.page.currentPage = 1
+      this.getAllMaterial()
+    },
     getAllMaterial () {
       this.$axios({
         url: '/user/images',
-        params: { collect: false }
+        params: {
+          collect: this.activeName === 'collect',
+          page: this.page.currentPage,
+          per_page: this.page.pageSize
+        }
       }).then(res => {
         this.list = res.data.results
+        this.page.total = res.data.total_count // 获取总条数
       })
     }
   },
@@ -58,14 +99,14 @@ export default {
       width: 100%;
       height: 100%;
     }
-    .operate{
-        width: 100%;
-        font-size: 20px;
-        position: absolute;
-        bottom: 0;
-        background-color: #f4f5f6;
-        height: 30px;
-        margin-left: -20px;
+    .operate {
+      width: 100%;
+      font-size: 20px;
+      position: absolute;
+      bottom: 0;
+      background-color: #f4f5f6;
+      height: 30px;
+      margin-left: -20px;
     }
   }
 }
