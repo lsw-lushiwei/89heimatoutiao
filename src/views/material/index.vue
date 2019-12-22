347d8e8c0fac19b8b00d41a1e3ca7a47ba72db05
lushiwei <lushiwei0822@163.com>
2019-12-22 19:57:17 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,17 +1,29 @@
 <template>
   <el-card>
-      <bread-crumb slot="header">
-        <template slot="title">素材管理</template>
-      </bread-crumb>
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <!-- 素材 -->
+    <el-tabs v-model="activeName">
+      <el-tab-pane label="全部素材" name="all">
+
+      </el-tab-pane>
+      <el-tab-pane label="收藏素材" name="collect">
+
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'collect'
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
