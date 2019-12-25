dcc07e491576bff255b3976a92329364263d31da
lushiwei <lushiwei0822@163.com>
2019-12-25 15:40:01 +0800

    发表文章-频道数据加载

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -8,16 +8,18 @@
         <el-input style="width: 50%"></el-input>
       </el-form-item>
       <el-form-item label="内容">
-        <el-input type="textarea" :rows="4" placeholder="请输入内容" v-model="textarea"></el-input>
+        <el-input type="textarea" :rows="4"></el-input>
       </el-form-item>
       <el-form-item label="封面">
-          <el-radio>单图</el-radio>
-          <el-radio>三图</el-radio>
-          <el-radio>无图</el-radio>
-          <el-radio>自动</el-radio>
+        <el-radio>单图</el-radio>
+        <el-radio>三图</el-radio>
+        <el-radio>无图</el-radio>
+        <el-radio>自动</el-radio>
       </el-form-item>
       <el-form-item label="频道">
-          <el-select></el-select>
+        <el-select>
+          <el-option v-for="item in channels" :key="item.id" :value="item.id" :label="item.name"></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary">发布</el-button>
@@ -28,7 +30,26 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      channels: [] // 定义一个channels接收频道
+    }
+  },
+  methods: {
+    // 获取频道
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(res => {
+        this.channels = res.data.channels // 获取频道数据
+      })
+    }
+  },
+  created () {
+    this.getChannels() // 获取频道数据
+  }
+}
 </script>
 
 <style>
