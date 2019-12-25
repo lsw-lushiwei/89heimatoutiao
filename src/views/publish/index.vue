8e97b23e326edcf8258638da2bb609e7d532b29d
lushiwei <lushiwei0822@163.com>
2019-12-25 15:26:21 +0800

    发表文章-页面结构-简单实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,17 +1,35 @@
 <template>
   <el-card>
-      <bread-crumb slot="header">
-        <template slot="title">发布文章</template>
-      </bread-crumb>
+    <bread-crumb slot="header">
+      <template slot="title">发布文章</template>
+    </bread-crumb>
+    <el-form style="margin-left: 50px" label-width="100px">
+      <el-form-item label="标题">
+        <el-input style="width: 50%"></el-input>
+      </el-form-item>
+      <el-form-item label="内容">
+        <el-input type="textarea" :rows="4" placeholder="请输入内容" v-model="textarea"></el-input>
+      </el-form-item>
+      <el-form-item label="封面">
+          <el-radio>单图</el-radio>
+          <el-radio>三图</el-radio>
+          <el-radio>无图</el-radio>
+          <el-radio>自动</el-radio>
+      </el-form-item>
+      <el-form-item label="频道">
+          <el-select></el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">发布</el-button>
+        <el-button>存入草稿</el-button>
+      </el-form-item>
+    </el-form>
   </el-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
