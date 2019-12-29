fd2160d57679fdf2763ffe0c3f160b005add10fb
lushiwei <lushiwei0822@163.com>
2019-12-29 16:17:35 +0800

    最后优化

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="not-found">
     <img src="../assets/img/404.gif" alt />
+    <div>找不到要访问的地址</div>
   </div>
 </template>
 
@@ -14,9 +15,16 @@ export default {}
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
   img {
     width: 800px;
     height: 450px;
+  }
+  div{
+    font-size: 50px;
+    color: pink;
+    position: absolute;
+    top: 100px;
   }
 }
 </style>
